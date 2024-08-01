717013e16df59b667c13c32a9808bdcc88e8b10c
Gabor Benko <benkogabor87@gmail.com>
2024-08-01 19:42:08 +0200

    Service

---
+++ MachineStreamer.Services/IMachineService.cs
@@ -0,0 +1,12 @@
+﻿using MachinStreamer.DAL.Models;
+using MongoDB.Driver;
+
+namespace MachineStreamer.Services
+{
+    public interface IMachineService
+    {
+        Task<ICollection<MachineData>> GetAllAsync(FilterDefinition<MachineData> filter);
+
+        Task InsertAsync(MachineData data);
+    }
+}
---
+++ MachineStreamer.Services/MachineService.cs
@@ -0,0 +1,25 @@
+﻿using MachinStreamer.DAL.Models;
+using MachinStreamer.DAL.Repositories;
+using MongoDB.Driver;
+
+namespace MachineStreamer.Services
+{
+    public class MachineService : IMachineService
+    {
+        private readonly IMachineDataRepository machineDataRepository;
+
+        public MachineService(IMachineDataRepository machineDataRepository)
+        {
+            this.machineDataRepository = machineDataRepository;
+        }
+        public Task<ICollection<MachineData>> GetAllAsync(FilterDefinition<MachineData> filter)
+        {
+            return machineDataRepository.GetAllAsync(filter);
+        }
+
+        public Task InsertAsync(MachineData data)
+        {
+            return machineDataRepository.InsertAsync(data);
+        }
+    }
+}
